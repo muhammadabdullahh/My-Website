e4e0d9e5ff7f7bb9cfee3f3ce5202304337d207a
theofficialmo <info.abdullahmuhammad@gmail.com>
2022-05-11 00:58:51 -0400

    .

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer", "Gym-Freak", "Student", "Learner"],
+        strings: ["Gamer", "Developer", "Student", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer", "Gym-Freak", "Student", "Learner"],
+        strings: ["Gamer", "Developer", "Student", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
