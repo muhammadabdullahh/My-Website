83abe0f8c1578486b9fbad2b7670d0af10544352
theofficialmo <info.abdullahmuhammad@gmail.com>
2024-05-01 20:20:26 -0400

    updates to portfolio

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer", "Student", "Learner"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer", "Student", "Learner"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
